b70db376648a09064c5d6d5ff8e7870b68e0f03b
dongyu1366 <dongyu1366@gmail.com>
2020-08-30 11:10:01 +0800

    Updated README.md

--- src/application.py
+++ src/application.py
@@ -1,9 +1,11 @@
+import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 import books
 
 
 app = Flask(__name__)
+port = int(os.environ.get("PORT", 5000))
 app.secret_key = 'dsfasdfqr32543trehgedfgsd'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///library.sqlite'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
@@ -16,4 +18,4 @@ from orm.models import *
 if __name__ == '__main__':
     db.create_all()
     books.insert_csv_to_db()
-    app.run(port=5000, host='0.0.0.0')
+    app.run(debug=True, host='0.0.0.0', port=port)
